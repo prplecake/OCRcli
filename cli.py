1fe01a3538df3452ad8156a1f82305f7a841e514
Matthew Jorgensen <matthew@jrgnsn.net>
2022-04-02 21:12:07 -0500

    Initial commit

---
+++ cli.py
@@ -0,0 +1,32 @@
+import gettext
+import sys
+
+from PIL import Image
+
+import pyocr
+
+try:
+    input_image = sys.argv[1]
+except IndexError:
+    print('No input image provided. Exiting...')
+    sys.exit(1)
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print(gettext.gettext("No OCR tools found. Please install tesseract-ocr and/or libtesseract."))
+    sys.exit(1)
+
+tool = tools[0]
+print(f'Using {tool.get_name()}')
+
+langs = tool.get_available_languages()
+langs.remove('osd')
+lang = 'eng'
+
+image = Image.open(input_image)
+
+print(f'Input image: {input_image}\n\n')
+
+out = tool.image_to_string(image, lang).replace("|", "I")
+
+print(out)
